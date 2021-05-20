9dcdb4128479b174e702e7dcd5eaf875aa31dd0a
André Cocuroci <andremartins@luizalabs.com>
2021-05-20 14:31:02 -0300

    Initial struct

---
+++ CoinTrack/CoinTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackApp.swift
+//  CoinTrack
+//
+//  Created by André Cocuroci Martins on 20/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
